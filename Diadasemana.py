661628320c2f846faeee28c7723fb1db5edfb1c4
Vinicius Ferreira <68194993+Viniciusfersan89@users.noreply.github.com>
2020-07-22 12:24:39 -0300

    Script Concluído
    Programa que verifica dia de trabalho ou dia de folga.

---
+++ Diadasemana.py
@@ -0,0 +1,12 @@
+
+dds=str(input('Qual é o dia da semana?\n'))
+
+if dds == 'sabado' or dds == 'Sabado' or dds =='sábado' or dds =='Sábado':
+ 	print('Pode descansar hoje é sua folga '+dds)
+
+elif dds=='domingo' or dds == 'Domingo': 
+ 	print('Pode descansar hoje é sua folga '+dds)
+
+else:
+ 	print('Hoje é seu dia de Trabalhar '+dds)
+ 
